37446a478a5f69709bba51bd21b65eaf005c5c99
Bartomeu Sales <bartomeu.sales@melia.com>
2020-04-28 17:41:01 +0200

    add desing activity profile

--- app/src/main/java/com/socib/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/socib/ui/profile/ProfileFragment.java
@@ -4,19 +4,19 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.socib.R;
-import com.socib.ui.notifications.NotificationsViewModel;
 
-public class ProfileFragment extends Fragment {
+public class ProfileFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private ProfileViewModel profileViewModel;
 
@@ -30,13 +30,36 @@ public class ProfileFragment extends Fragment {
         final TextView mTitle = toolbar.findViewById(R.id.toolbar_title);
         mTitle.setText(R.string.title_profile);
 
-        final TextView textView = root.findViewById(R.id.text_profile);
-        profileViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        Spinner spinnerLanguage = root.findViewById(R.id.language_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapterLanguage = ArrayAdapter.createFromResource(getActivity(),
+                R.array.language_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapterLanguage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerLanguage.setAdapter(adapterLanguage);
+
+        Spinner spinnerProfile = root.findViewById(R.id.profile_spinner);
+        ArrayAdapter<CharSequence> adapterProfile = ArrayAdapter.createFromResource(getActivity(),
+                R.array.profile_array, android.R.layout.simple_spinner_item);
+        adapterProfile.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerProfile.setAdapter(adapterProfile);
+
+        Spinner spinnerUnits = root.findViewById(R.id.units_spinner);
+        ArrayAdapter<CharSequence> adapterUnits = ArrayAdapter.createFromResource(getActivity(),
+                R.array.units_array, android.R.layout.simple_spinner_item);
+        adapterUnits.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerUnits.setAdapter(adapterUnits);
         return root;
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
