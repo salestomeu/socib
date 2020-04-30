ec6f5b5daf7cf9e0691e20436acaaa309071ede2
Bartomeu Sales <bartomeu.sales@melia.com>
2020-04-30 17:33:49 +0200

    addProfile

--- app/src/main/java/com/socib/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/socib/ui/profile/ProfileFragment.java
@@ -3,7 +3,6 @@ package com.socib.ui.profile;
 import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +14,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.socib.R;
 import com.socib.service.profile.ProfileService;
@@ -34,9 +34,6 @@ public class ProfileFragment extends Fragment {
         mTitle.setText(R.string.title_profile);
 
         profileService = ProfileService.getInstance();
-
-        setSpinnerLanguage(root.findViewById(R.id.language_spinner));
-
         Spinner spinnerProfile = root.findViewById(R.id.profile_spinner);
         ArrayAdapter<CharSequence> adapterProfile = ArrayAdapter.createFromResource(getActivity(),
                 R.array.profile_array, android.R.layout.simple_spinner_item);
@@ -46,38 +43,6 @@ public class ProfileFragment extends Fragment {
         setSpinnerUnits(root.findViewById(R.id.units_spinner));
 
         return root;
-    }
-
-    private void setSpinnerLanguage(Spinner spinnerLanguage) {
-        ArrayAdapter<CharSequence> adapterLanguage = ArrayAdapter.createFromResource(getActivity(),
-                R.array.language_array, android.R.layout.simple_spinner_item);
-        adapterLanguage.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerLanguage.setAdapter(adapterLanguage);
-        spinnerLanguage.setSelection(getPositionLanguage());
-        spinnerLanguage.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        profileService.setLanguage("en");
-                        setAppLocale("en");
-                        break;
-                    case 1:
-                        profileService.setLanguage("es");
-                        setAppLocale("es");
-                        break;
-                    case 2:
-                        profileService.setLanguage("ca");
-                        setAppLocale("ca");
-                        break;
-                }
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
     }
 
     private void setSpinnerUnits(Spinner spinnerUnits) {
@@ -108,21 +73,6 @@ public class ProfileFragment extends Fragment {
             }
         });
     }
-    private int getPositionLanguage() {
-        int result = 0;
-        switch (profileService.getProfile().getLanguage()) {
-            case "en":
-                result = 0;
-                break;
-            case "es":
-                result = 1;
-                break;
-            case "ca":
-                result = 2;
-                break;
-        }
-        return result;
-    }
 
     private int getPositionUnits() {
         int result = 0;
@@ -140,10 +90,4 @@ public class ProfileFragment extends Fragment {
         return result;
     }
 
-    private void setAppLocale(String localeCode) {
-        Resources res = getResources();
-        Configuration conf = res.getConfiguration();
-        conf.setLocale(new Locale(localeCode));
-        res.updateConfiguration(conf, res.getDisplayMetrics());
-    }
 }
